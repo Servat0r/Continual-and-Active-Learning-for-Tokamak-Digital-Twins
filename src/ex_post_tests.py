Further fixes and changes. Added new utilities to ex_post_tests.py and changed input and output normalization to be based ONLY on first experience data.

src/ex_post_tests.py
@@ -1,83 +1,136 @@
 # Tests for results after training
 import json
 import os.path
-import pandas as pd
-from src.utils import *
-from sklearn.model_selection import train_test_split
+from pathlib import Path
+
 import torch
+import pandas as pd
+
+from src.utils import *
 
 
-def load_models_and_raw_data(log_folder: str, load_intermediate_models: bool = False, test_size=0.2):
-    with open(os.path.join(log_folder, 'config.json'), 'r') as fp:
-        config = json.load(fp)
-    dtype = config['dtype']
-    pow_type = config['pow_type']
-    cluster_type = config['cluster_type']
-    dataset_type = config['dataset_type']
-    task = config['task']
-    num_campaigns = config['num_campaigns']
-    normalize_inputs = config['normalize_inputs']
-    normalize_outputs = config['normalize_outputs']
-
-    model = torch.load(os.path.join(log_folder, 'model.pt'))
-    intermediate_models = {}
-    if load_intermediate_models:
-        for i in range(config['num_campaigns']):
-            intermediate_models[i] = torch.load(os.path.join(log_folder, f'model_after_exp_{i}.pt'))
-    csv_file = f'data/baseline/cleaned/{pow_type}_cluster/{cluster_type}/{dataset_type}_dataset.csv'
-    float_precision = dtype
-    dtype = get_dtype_from_str(dtype)
-    OUTPUTS = BASELINE_HIGHPOW_OUTPUTS if task == 'regression' else ['has_turbulence']
-    data = pd.read_csv(csv_file)
-    print(len(data))
-    # Split the data into train and test sets
-    train_data, test_data = train_test_split(data, test_size=test_size, random_state=42)
-    transform = None
-    target_transform = None
+def get_normalization_transforms(
+        data, input_columns, output_columns, transform=None, target_transform=None, dtype=float32,
+        normalize_inputs: bool = True, normalize_outputs: bool = False,
+):
     if normalize_inputs:
-        inputs_mean = torch.load(os.path.join(log_folder, "input_mean.pt"))
-        inputs_std = torch.load(os.path.join(log_folder, "input_std.pt"))
-        transform = CustomNormalize(inputs_mean, inputs_std)
+        norm_transform, (mean, std) = build_normalization_transforms(data, input_columns, dtype)
+        transform = transforms.Compose([norm_transform, transform]) if transform else norm_transform
     if normalize_outputs:
-        outputs_mean = torch.load(os.path.join(log_folder, "output_mean.pt"))
-        outputs_std = torch.load(os.path.join(log_folder, "output_std.pt"))
-        target_transform = CustomNormalize(outputs_mean, outputs_std)
-    stuff = {
-        'model': model,
-        'intermediate_models': intermediate_models,
-        'train_data': train_data,
-        'test_data': test_data,
-        'dtype': dtype,
-        'float_precision': float_precision,
-        'OUTPUTS': OUTPUTS,
-        'transform': transform,
-        'target_transform': target_transform,
-        'num_campaigns': num_campaigns,
-    }
-    return stuff
+        norm_target_transform, (mean, std) = build_normalization_transforms(data, output_columns, dtype)
+        target_transform = transforms.Compose([norm_target_transform, target_transform]) \
+            if target_transform else norm_target_transform
+    return transform, target_transform
+
+
+def load_baseline_csv_data(
+        pow_type: str, cluster_type: str, dataset_type: str = 'not_null',
+        raw_or_final: str = 'final', train: bool = True,
+        validation: bool = True, test: bool = True, task: str = 'regression',
+):
+    data_folder = f'data/baseline/cleaned/{pow_type}_cluster/{cluster_type}'
+    train_filename = f'{raw_or_final}_train_data_{task}.csv'
+    eval_filename = f'{raw_or_final}_eval_data_{task}.csv'
+    test_filename = f'{raw_or_final}_test_data_{task}.csv'
+    train_data, eval_data, test_data = None, None, None
+    if train:
+        train_data = pd.read_csv(f'{data_folder}/{train_filename}')
+    if validation:
+        eval_data = pd.read_csv(f'{data_folder}/{eval_filename}')
+    if test:
+        test_data = pd.read_csv(f'{data_folder}/{test_filename}')
+    return train_data, eval_data, test_data
+
+
+def get_baseline_tensor_data(
+        data: pd.DataFrame | dict[str, pd.DataFrame],
+        input_columns: list[str], output_columns: list[str],
+):
+    if not isinstance(data, dict):
+        data = {'default': data}
+    tensor_data = {}
+    for key, df in data.items():
+        ...
+
+
+def load_models(
+        pow_type: str, cluster_type: str, dataset_type: str = 'not_null',
+        task: str = 'regression', outputs: str = 'efe_efi_pfe_pfi',
+        strategy: str = 'Naive', extra_log_folder: str = 'Base',
+        task_ids: int | list[int] = 0,
+):
+    base_log_folder = \
+        f'logs/{pow_type}/{cluster_type}/{task}/{dataset_type}/{outputs}/{strategy}/{extra_log_folder}'
+    if not isinstance(task_ids, list):
+        task_ids = [task_ids]
+    task_ids.sort()
+    path = Path(base_log_folder)
+    directories = [
+        os.path.join(base_log_folder, d.name) for d in path.iterdir() if d.is_dir()
+    ]
+    # Order as task_0, task_1, task_2 etc
+    directories = sorted(directories, key=lambda x: int(x[-1]))
+    models = {}
+    for task_id in task_ids:
+        state_dict = torch.load(os.path.join(directories[task_id], 'model.pt'))
+        config_filename = os.path.join(directories[task_id], 'config.json')
+        config = json.load(open(config_filename))
+        model_parameters = config['architecture']['parameters']
+        model_class = SimpleRegressionMLP if task == 'regression' else SimpleClassificationMLP # TODO REWORK TO INCLUDE OTHER MODELS
+        model = model_class(**model_parameters)
+        model.load_state_dict(state_dict)
+        models[task_id] = model
+    return models
+
 
 def build_full_datasets(
-    train_data, test_data, input_columns, output_columns, transform, target_transform, float_precision, device
+    train_data: pd.DataFrame, eval_data: pd.DataFrame, test_data: pd.DataFrame,
+    input_columns: list[str] = BASELINE_HIGHPOW_INPUTS, output_columns: list[str] = BASELINE_HIGHPOW_OUTPUTS,
+    transform = None, target_transform = None, float_precision: str = 'float32', device: str = 'cpu',
+    normalize_inputs: bool = True, normalize_outputs: bool = False,
 ):
+    dtype = get_dtype_from_str(float_precision)
+    transform, target_transform = get_normalization_transforms(
+        train_data, input_columns, output_columns, transform=transform, target_transform=target_transform,
+        dtype=dtype, normalize_inputs=normalize_inputs, normalize_outputs=normalize_outputs,
+    )
     full_train_dataset = CSVRegressionDataset(
         train_data, input_columns=input_columns, output_columns=output_columns, transform=transform,
+        target_transform=target_transform, filter_by=None, float_precision=float_precision, device=device,
+    )
+    full_eval_dataset = CSVRegressionDataset(
+        eval_data, input_columns=input_columns, output_columns=output_columns, transform=transform,
         target_transform=target_transform, filter_by=None, float_precision=float_precision, device=device,
     )
     full_test_dataset = CSVRegressionDataset(
         test_data, input_columns=input_columns, output_columns=output_columns, transform=transform,
         target_transform=target_transform, filter_by=None, float_precision=float_precision, device=device,
     )
-    return full_train_dataset, full_test_dataset
+    return full_train_dataset, full_eval_dataset, full_test_dataset
+
 
 def build_experience_datasets(
-    train_data, test_data, input_columns, output_columns, transform, target_transform, float_precision,
-    device, num_campaigns,
+    train_data: pd.DataFrame, eval_data: pd.DataFrame, test_data: pd.DataFrame,
+    input_columns: list[str] = BASELINE_HIGHPOW_INPUTS, output_columns: list[str] = BASELINE_HIGHPOW_OUTPUTS,
+    transform = None, target_transform = None, float_precision: str = 'float32', device: str = 'cpu',
+    num_campaigns: int = 10, normalize_inputs: bool = True, normalize_outputs: bool = False,
 ):
+    dtype = get_dtype_from_str(float_precision)
+    transform, target_transform = get_normalization_transforms(
+        train_data, input_columns, output_columns, transform=transform, target_transform=target_transform,
+        dtype=dtype, normalize_inputs=normalize_inputs, normalize_outputs=normalize_outputs,
+    )
     experience_train_datasets = {}
+    experience_eval_datasets = {}
     experience_test_datasets = {}
     for campaign in range(num_campaigns):
         train_dataset = CSVRegressionDataset(
             train_data, input_columns=input_columns, output_columns=output_columns, transform=transform,
+            target_transform=target_transform, filter_by={'campaign': [campaign]}, float_precision=float_precision,
+            device=device,
+        )
+        eval_dataset = CSVRegressionDataset(
+            eval_data, input_columns=input_columns, output_columns=output_columns, transform=transform,
             target_transform=target_transform, filter_by={'campaign': [campaign]}, float_precision=float_precision,
             device=device,
         )
@@ -87,25 +140,12 @@ def build_experience_datasets(
             device=device,
         )
         experience_train_datasets[campaign] = train_dataset
+        experience_eval_datasets[campaign] = eval_dataset
         experience_test_datasets[campaign] = test_dataset
-    return experience_train_datasets, experience_test_datasets
+    return experience_train_datasets, experience_eval_datasets, experience_test_datasets
 
 
-#### Main workload functions
-def full_models_and_datasets_load(log_folder: str, load_intermediate_models: bool = False, test_size=0.2):
-    stuff = load_models_and_raw_data(log_folder, load_intermediate_models, test_size)
-    model = stuff['model']
-    intermediate_models = stuff['intermediate_models']
-    train_data = stuff['train_data']
-    test_data = stuff['test_data']
-    float_precision = stuff['float_precision']
-    OUTPUTS = stuff['OUTPUTS']
-    transform = stuff['transform']
-    target_transform = stuff['target_transform']
-    num_campaigns = stuff['num_campaigns']
-    device = 'cpu'
-    full_train_dataset, full_test_dataset = build_full_datasets(
-        train_data, test_data, BASELINE_HIGHPOW_INPUTS, OUTPUTS, transform, target_transform,
-        float_precision, device,
-    )
-    return model, intermediate_models, full_train_dataset, full_test_dataset
+__all__ = [
+    'load_models', 'load_baseline_csv_data',
+    'build_full_datasets', 'build_experience_datasets',
+]

src/utils/benchmarks.py
@@ -13,7 +13,7 @@ from .datasets import *
 from .transforms import *
 
 
-def _build_normalization_transforms(data, columns, dtype, transform=None):
+def build_normalization_transforms(data, columns, dtype, transform=None):
     mean = torch.tensor(data[columns].mean(axis=0), dtype=dtype)
     std = torch.tensor(data[columns].std(axis=0), dtype=dtype)
     print(f"[green]mean = {mean}, std = {std} ...[/green]")
@@ -101,14 +101,15 @@ def make_benchmark(
         )
     if task == 'classification':
         output_columns = ['has_turbulence']
+    first_exp_train_data = train_data[train_data.campaign == 0]
     if normalize_inputs:
-        norm_transform, (mean, std) = _build_normalization_transforms(train_data, input_columns, dtype)
+        norm_transform, (mean, std) = build_normalization_transforms(first_exp_train_data, input_columns, dtype)
         if log_folder:
             torch.save(mean, os.path.join(log_folder, "input_mean.pt"))
             torch.save(std, os.path.join(log_folder, "input_std.pt"))
         transform = transforms.Compose([norm_transform, transform]) if transform else norm_transform
     if normalize_outputs:
-        norm_target_transform, (mean, std) = _build_normalization_transforms(train_data, output_columns, dtype)
+        norm_target_transform, (mean, std) = build_normalization_transforms(first_exp_train_data, output_columns, dtype)
         if log_folder:
             torch.save(mean, os.path.join(log_folder, "output_mean.pt"))
             torch.save(std, os.path.join(log_folder, "output_std.pt"))
@@ -139,3 +140,8 @@ def make_benchmark(
         test_datasets.append(test_dataset)
     benchmark = benchmark_from_datasets(train=train_datasets, eval=eval_datasets, test=test_datasets)
     return benchmark
+
+
+__all__ = [
+    'build_normalization_transforms', 'subsample', 'make_benchmark'
+]

src/utils/plots.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,20 +8,29 @@ from .misc import extract_metric_values_over_evaluation_experiences
 
 def plot_metric_over_evaluation_experiences(
         file_path_or_buf: str | pd.DataFrame, metric: str, title: str, xlabel: str, ylabel: str,
-        grid: bool = True, legend: bool = True, show: bool = True, start_exp: int = 0, end_exp: int = -1,
-        save: bool = True, savepath: str = None, num_exp: int = None,
+        grid: bool = True, legend: bool = True, show: bool = True, experiences: Iterable[int] = None,
+        save: bool = True, savepath: str = None, num_exp: int = None, from_beginning: bool = True,
 ):
     df: pd.DataFrame = pd.read_csv(file_path_or_buf) if isinstance(file_path_or_buf, str) else file_path_or_buf
     default_num_exp = len(df['eval_exp'].unique())
     num_exp = default_num_exp if num_exp is None else num_exp
-    if (end_exp == -1) or (end_exp >= num_exp): end_exp = default_num_exp - 1
+    if experiences is None:
+        experiences = range(default_num_exp)
     dict_data = {}
-    for eval_exp in range(start_exp, end_exp + 1):
+    for eval_exp in experiences:
         value = df[df['eval_exp'] == eval_exp][metric].to_numpy()
         dict_data[f"Eval Experience {eval_exp}"] = value
     ddf = pd.DataFrame(dict_data)
     plt.figure(figsize=(12, 8))
-    ddf.plot(kind='line', marker='o', linestyle='-')
+    if from_beginning:
+        ddf.plot(kind='line', marker='o', linestyle='-')
+    else:
+        for eval_exp in experiences:
+            column = f"Eval Experience {eval_exp}"
+            plt.plot(
+                ddf.index[eval_exp:], ddf[column][eval_exp:], label=column,
+                kind='line', marker='o', linestyle='-'
+            )
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -33,20 +43,21 @@ def plot_metric_over_evaluation_experiences(
 
 def plot_metrics_over_training_experiences(
         file_path_or_buf: str | pd.DataFrame, metric: str, title: str, xlabel: str, ylabel: str,
-        grid: bool = True, legend: bool = True, show: bool = True, start_exp: int = 0, end_exp: int = -1,
+        grid: bool = True, legend: bool = True, show: bool = True, experiences: Iterable[int] = None,
         save: bool = True, savepath: str = None, num_exp: int = None,
 ):
     df = pd.read_csv(file_path_or_buf) if isinstance(file_path_or_buf, str) else file_path_or_buf
     default_num_exp = len(df['eval_exp'].unique())
     num_exp = default_num_exp if num_exp is None else num_exp
-    if (end_exp == -1) or (end_exp >= num_exp): end_exp = default_num_exp - 1
+    if experiences is None:
+        experiences = range(default_num_exp)
     num_epochs = len(df['epoch'].unique())
     print(num_exp, num_epochs)
     ddf = pd.DataFrame({'epoch': np.arange(num_epochs)})
-    for training_exp in range(start_exp, end_exp + 1):
+    for training_exp in experiences:
         selected_df = df[df['training_exp'] == training_exp][metric]
         ddf[f"Training Exp {training_exp}"] = selected_df
-        plt.plot(np.arange(num_epochs), selected_df, label=f"Training Exp {training_exp}")
+        plt.plot(ddf.index, selected_df, label=f"Training Exp {training_exp}")
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -58,17 +69,18 @@ def plot_metrics_over_training_experiences(
 
 def plot_metric_over_evaluation_experiences_multiple_runs(
         file_paths_or_bufs: list[str | pd.DataFrame], metric: str, title: str, xlabel: str, ylabel: str,
-        grid: bool = True, legend: bool = True, show: bool = True, start_exp: int = 0, end_exp: int = -1,
-        save: bool = True, savepath: str = None, num_exp: int = None,
+        grid: bool = True, legend: bool = True, show: bool = True, experiences: Iterable[int] = None,
+        save: bool = True, savepath: str = None, num_exp: int = None, from_beginning: bool = True,
 ):
     dfs: list[pd.DataFrame] = [pd.read_csv(fp) if isinstance(fp, str) else fp for fp in file_paths_or_bufs]
     default_num_exp = len(dfs[0]['eval_exp'].unique())
     num_exp = default_num_exp if num_exp is None else num_exp
-    if (end_exp == -1) or (end_exp >= num_exp): end_exp = default_num_exp - 1
+    if experiences is None:
+        experiences = range(default_num_exp)
     ddfs = []
     for df in dfs:
         dict_data = {}
-        for eval_exp in range(start_exp, end_exp + 1):
+        for eval_exp in experiences:
             value = df[df['eval_exp'] == eval_exp][metric].to_numpy()
             dict_data[f"Eval Experience {eval_exp}"] = value
         ddf = pd.DataFrame(dict_data)
@@ -77,17 +89,19 @@ def plot_metric_over_evaluation_experiences_multiple_runs(
     plt.figure(figsize=(12, 8))
     means = {}
     stds = {}
-    for column in ddfs[0].columns[start_exp:end_exp + 1]:
+    for column in ddfs[0].columns:
         stacked = np.vstack([ ddf[column] for ddf in ddfs ])
         means[column] = stacked.mean(axis=0)
         stds[column] = stacked.std(axis=0)
-    for column, mean_values in means.items():
+    for idx, (column, mean_values) in enumerate(means.items()):
         std_vals = stds[column]
+        index = 0 if from_beginning else idx
         plt.plot(
-            x_values, mean_values, label=column, marker='o', linestyle='-'
+            x_values[index:], mean_values[index:], label=column, marker='o', linestyle='-'
         )
         plt.fill_between(
-            x_values, mean_values - std_vals, mean_values + std_vals, alpha=0.2
+            x_values[index:], (mean_values - std_vals)[index:],
+            (mean_values + std_vals)[index:], alpha=0.2
         )
     plt.title(title)
     plt.xlabel(xlabel)